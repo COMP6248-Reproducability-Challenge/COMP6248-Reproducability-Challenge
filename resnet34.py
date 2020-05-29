f3ac37afed9352e976cd47ceecd628b8ea7cfb95
yyss1997 <51158835+yyss1997@users.noreply.github.com>
2020-05-29 17:39:01 -0500

    Add files via upload

---
+++ resnet34.py
@@ -0,0 +1,368 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+import torch
+from __future__ import print_function
+import torch.optim as optim
+import torch.backends.cudnn as cudnn
+import torchvision
+import torchvision.transforms as transforms
+import os
+import argparse
+from adabound import AdaBound
+
+
+def ResNet34():
+    return ResNet(BasicBlock, [3, 4, 6, 3])
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512 * block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride,
+                          bias=False),
+                nn.BatchNorm2d(self.expansion * planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class Transition(nn.Module):
+    def __init__(self, in_planes, out_planes):
+        super(Transition, self).__init__()
+        self.bn = nn.BatchNorm2d(in_planes)
+        self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=1, bias=False)
+
+    def forward(self, x):
+        out = self.conv(F.relu(self.bn(x)))
+        out = F.avg_pool2d(out, 2)
+        return out
+
+
+class DenseNet(nn.Module):
+    def __init__(self, block, nblocks, growth_rate=12, reduction=0.5, num_classes=10):
+        super(DenseNet, self).__init__()
+        self.growth_rate = growth_rate
+
+        num_planes = 2 * growth_rate
+        self.conv1 = nn.Conv2d(3, num_planes, kernel_size=3, padding=1, bias=False)
+
+        self.dense1 = self._make_dense_layers(block, num_planes, nblocks[0])
+        num_planes += nblocks[0] * growth_rate
+        out_planes = int(math.floor(num_planes * reduction))
+        self.trans1 = Transition(num_planes, out_planes)
+        num_planes = out_planes
+
+        self.dense2 = self._make_dense_layers(block, num_planes, nblocks[1])
+        num_planes += nblocks[1] * growth_rate
+        out_planes = int(math.floor(num_planes * reduction))
+        self.trans2 = Transition(num_planes, out_planes)
+        num_planes = out_planes
+
+        self.dense3 = self._make_dense_layers(block, num_planes, nblocks[2])
+        num_planes += nblocks[2] * growth_rate
+        out_planes = int(math.floor(num_planes * reduction))
+        self.trans3 = Transition(num_planes, out_planes)
+        num_planes = out_planes
+
+        self.dense4 = self._make_dense_layers(block, num_planes, nblocks[3])
+        num_planes += nblocks[3] * growth_rate
+
+        self.bn = nn.BatchNorm2d(num_planes)
+        self.linear = nn.Linear(num_planes, num_classes)
+
+    def _make_dense_layers(self, block, in_planes, nblock):
+        layers = []
+        for i in range(nblock):
+            layers.append(block(in_planes, self.growth_rate))
+            in_planes += self.growth_rate
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.trans1(self.dense1(out))
+        out = self.trans2(self.dense2(out))
+        out = self.trans3(self.dense3(out))
+        out = self.dense4(out)
+        out = F.avg_pool2d(F.relu(self.bn(out)), 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+def DenseNet121():
+    return DenseNet(Bottleneck, [6, 12, 24, 16], growth_rate=32)
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+    parser.add_argument('--model', default='resnet', type=str, help='model',
+                        choices=['resnet', 'densenet'])
+    parser.add_argument('--optim', default='sgd', type=str, help='optimizer',
+                        choices=['sgd', 'adagrad', 'adam', 'amsgrad', 'adabound', 'amsbound'])
+    parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+    parser.add_argument('--final_lr', default=0.1, type=float,
+                        help='final learning rate of AdaBound')
+    parser.add_argument('--gamma', default=1e-3, type=float,
+                        help='convergence speed term of AdaBound')
+    parser.add_argument('--momentum', default=0.9, type=float, help='momentum term')
+    parser.add_argument('--beta1', default=0.9, type=float, help='Adam coefficients beta_1')
+    parser.add_argument('--beta2', default=0.999, type=float, help='Adam coefficients beta_2')
+    parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+    parser.add_argument('--weight_decay', default=5e-4, type=float,
+                        help='weight decay for optimizers')
+    return parser
+
+
+class Bottleneck(nn.Module):
+    def __init__(self, in_planes, growth_rate):
+        super(Bottleneck, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.conv1 = nn.Conv2d(in_planes, 4 * growth_rate, kernel_size=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(4 * growth_rate)
+        self.conv2 = nn.Conv2d(4 * growth_rate, growth_rate, kernel_size=3, padding=1, bias=False)
+
+    def forward(self, x):
+        out = self.conv1(F.relu(self.bn1(x)))
+        out = self.conv2(F.relu(self.bn2(out)))
+        out = torch.cat([out, x], 1)
+        return out
+
+
+def build_dataset():
+    print('==> Preparing data..')
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True,
+                                            transform=transform_train)
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True,
+                                               num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True,
+                                           transform=transform_test)
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+
+
+    return train_loader, test_loader
+
+
+def get_ckpt_name(model='resnet', optimizer='sgd', lr=0.1, final_lr=0.1, momentum=0.9,
+                  beta1=0.9, beta2=0.999, gamma=1e-3):
+    name = {
+        'sgd': 'lr{}-momentum{}'.format(lr, momentum),
+        'adagrad': 'lr{}'.format(lr),
+        'adam': 'lr{}-betas{}-{}'.format(lr, beta1, beta2),
+        'amsgrad': 'lr{}-betas{}-{}'.format(lr, beta1, beta2),
+        'adabound': 'lr{}-betas{}-{}-final_lr{}-gamma{}'.format(lr, beta1, beta2, final_lr, gamma),
+        'amsbound': 'lr{}-betas{}-{}-final_lr{}-gamma{}'.format(lr, beta1, beta2, final_lr, gamma),
+    }[optimizer]
+    return '{}-{}-{}'.format(model, optimizer, name)
+
+
+def load_checkpoint(ckpt_name):
+    print('==> Resuming from checkpoint..')
+    path = os.path.join('checkpoint', ckpt_name)
+    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+    assert os.path.exists(path), 'Error: checkpoint {} not found'.format(ckpt_name)
+    return torch.load(ckpt_name)
+
+
+def build_model(args, device, ckpt=None):
+    print('==> Building model..')
+    net = {
+        'resnet': ResNet34,
+        'densenet': DenseNet121,
+    }[args.model]()
+    net = net.to(device)
+    if device == 'cuda':
+        net = torch.nn.DataParallel(net)
+        cudnn.benchmark = True
+
+    if ckpt:
+        net.load_state_dict(ckpt['net'])
+
+    return net
+
+
+def create_optimizer(args, model_params):
+    if args.optim == 'sgd':
+        return optim.SGD(model_params, args.lr, momentum=args.momentum,
+                         weight_decay=args.weight_decay)
+    elif args.optim == 'adagrad':
+        return optim.Adagrad(model_params, args.lr, weight_decay=args.weight_decay)
+    elif args.optim == 'adam':
+        return optim.Adam(model_params, args.lr, betas=(args.beta1, args.beta2),
+                          weight_decay=args.weight_decay)
+    elif args.optim == 'amsgrad':
+        return optim.Adam(model_params, args.lr, betas=(args.beta1, args.beta2),
+                          weight_decay=args.weight_decay, amsgrad=True)
+    elif args.optim == 'adabound':
+        return AdaBound(model_params, args.lr, betas=(args.beta1, args.beta2),
+                        final_lr=args.final_lr, gamma=args.gamma,
+                        weight_decay=args.weight_decay)
+    else:
+        assert args.optim == 'amsbound'
+        return AdaBound(model_params, args.lr, betas=(args.beta1, args.beta2),
+                        final_lr=args.final_lr, gamma=args.gamma,
+                        weight_decay=args.weight_decay, amsbound=True)
+
+
+def train(net, epoch, device, data_loader, optimizer, criterion):
+    print('\nEpoch: %d' % epoch)
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(data_loader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+    accuracy = 100. * correct / total
+    print('train acc %.3f' % accuracy)
+
+    return accuracy
+
+
+def test(net, device, data_loader, criterion):
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(data_loader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+    accuracy = 100. * correct / total
+    print(' test acc %.3f' % accuracy)
+
+    return accuracy
+
+
+if __name__ == '__main__':
+    parser = get_parser()
+    args = parser.parse_args()
+
+    train_loader, test_loader = build_dataset()
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    ckpt_name = get_ckpt_name(model=args.model, optimizer=args.optim, lr=args.lr,
+                              final_lr=args.final_lr, momentum=args.momentum,
+                              beta1=args.beta1, beta2=args.beta2, gamma=args.gamma)
+    if args.resume:
+        ckpt = load_checkpoint(ckpt_name)
+        best_acc = ckpt['acc']
+        start_epoch = ckpt['epoch']
+    else:
+        ckpt = None
+        best_acc = 0
+        start_epoch = -1
+
+    net = build_model(args, device, ckpt=ckpt)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = create_optimizer(args, net.parameters())
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=150, gamma=0.1,
+                                          last_epoch=start_epoch)
+
+    train_accuracies = []
+    test_accuracies = []
+
+    for epoch in range(start_epoch + 1, 200):
+        scheduler.step()
+        train_acc = train(net, epoch, device, train_loader, optimizer, criterion)
+        test_acc = test(net, device, test_loader, criterion)
+
+        # Save checkpoint.
+        if test_acc > best_acc:
+            print('Saving..')
+            state = {
+                'net': net.state_dict(),
+                'acc': test_acc,
+                'epoch': epoch,
+            }
+            if not os.path.isdir('checkpoint'):
+                os.mkdir('checkpoint')
+            torch.save(state, os.path.join('checkpoint', ckpt_name))
+            best_acc = test_acc
+
+        train_accuracies.append(train_acc)
+        test_accuracies.append(test_acc)
+        if not os.path.isdir('curve'):
+            os.mkdir('curve')
+        torch.save({'train_acc': train_accuracies, 'test_acc': test_accuracies},
+                   os.path.join('curve', ckpt_name))
